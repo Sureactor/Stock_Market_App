f10e134801b294b41e1be17e4b28585519e91f2d
Surendar <suredirector1998@gmail.com>
2020-08-28 11:17:08 +0530

    Deleted

--- Stock/src/main/java/com/StockApp/StockExchange/sector/SectorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.StockApp.StockExchange.sector;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.StockApp.StockExchange.StockExchange;
-
-public interface SectorRepository extends CrudRepository<Sector,Integer> {
-
-}
