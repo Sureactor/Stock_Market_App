cef0650d5a1a63e27d5c34243e0f5acdd886e387
ranjithasankar <34063668+ranjithasankar@users.noreply.github.com>
2020-08-30 23:07:46 +0530

    Added user functionality

---
+++ Stock/src/main/java/com/StockApp/User/Admin.java
@@ -0,0 +1,31 @@
+package com.StockApp.User;
+
+public class Admin {
+
+	String username;
+	String password;
+	
+	
+	public Admin() {
+		super();
+	}
+	public Admin(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
---
+++ Stock/src/main/java/com/StockApp/User/UserRepository.java
@@ -0,0 +1,14 @@
+package com.StockApp.User;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Integer> {
+
+	User findByUsernameAndPassword(String username, String password);
+
+	User findByUsername(String username);
+}
+
+
