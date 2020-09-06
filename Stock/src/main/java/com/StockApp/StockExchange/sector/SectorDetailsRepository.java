ca90710a0838995b363720c596cb1812f0e86f59
Surendar <suredirector1998@gmail.com>
2020-09-06 18:37:50 +0530

    del

--- Stock/src/main/java/com/StockApp/StockExchange/sector/SectorDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.StockApp.StockExchange.sector;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.StockApp.StockExchange.StockExchange;
-
-public interface SectorDetailsRepository extends CrudRepository<SectorDetails,Integer> {
-
-}
--- Stock/src/main/java/com/StockApp/StockExchange/sector/company/ipo/CompanyIPORepository.java
+++
@@ -1,9 +0,0 @@
-package com.StockApp.StockExchange.sector.company.ipo;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CompanyIPORepository extends CrudRepository<CompanyIPO,Integer> {
-	public List<CompanyIPO> findByCompanyCompanyId(Integer companyId);
-}
